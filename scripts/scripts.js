b7c5fdd5ed37d3dc2e586801393bbcfbaeeff621
JauniusPinelis <JauniusPinelis@gmail.com>
2016-12-07 08:42:30 +0200

    Moved the script functionality outside html and centered the grid

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+      //To be added
+      for (var x= 0; x < 10; x++){
+        for (var y = 0; y < 10; y++){
+          var unit = $("<div class='unit'></div>");
+          unit.appendTo('#grid');
+        }
+      }
+  });
