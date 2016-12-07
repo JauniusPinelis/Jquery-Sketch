4d57cda943c59ab812ad3c6373a70e0484f38c3a
JauniusPinelis <JauniusPinelis@gmail.com>
2016-12-07 09:17:42 +0200

    Some refactoring + added reset functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,9 +1,24 @@
 $( document ).ready(function() {
-      //To be added
-      for (var x= 0; x < 10; x++){
-        for (var y = 0; y < 10; y++){
-          var unit = $("<div class='unit'></div>");
-          unit.appendTo('#grid');
-        }
-      }
+      populateGrid();
+      addResetGridFunction();
   });
+
+populateGrid = function () {
+  for (var x= 0; x < 10; x++){
+    for (var y = 0; y < 10; y++){
+      var unit = $("<div class='unit'></div>");
+      unit.appendTo('#grid');
+    }
+  }
+  $('.unit').on('mouseenter', function(){
+    $(this).css({'background-color': 'black'});
+  });
+}
+
+addResetGridFunction= function () {
+  $(function(){
+    $('.resetButton').click(function() {
+        $('.unit').css({'background-color': 'blue'});
+    });
+  });
+}
